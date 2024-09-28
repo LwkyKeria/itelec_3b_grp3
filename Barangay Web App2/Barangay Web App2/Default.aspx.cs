64aef61db5d5d69bca7244a1bae7b486b95dd46b
Kim Andrei <kimandrei012@gmail.com>
2024-09-28 14:29:52 +0800

    latest web app

---
+++ Barangay Web App2/Barangay Web App2/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Barangay_Web_App2
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            String user = txtName.Text;
+            String pass = txtPassword.Text;
+
+            if ((user == "Kim") && (pass == "Besmar"))
+            {
+                Response.Redirect("Home.aspx");
+
+            }
+            else
+            {
+                lblResult.Text = "Tanginamo sino ka";
+            }
+                    
+        }
+    }
+}
